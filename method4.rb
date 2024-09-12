7213bd2abd2d1c4a4ba9b40ddde75e60fdf7e6c7
Aidan Alexander <alexander.1251@buckeyemail.osu.edu>
2024-09-12 14:49:44 -0400

    category 1 method 4 additions

---
+++ method4.rb
@@ -0,0 +1,17 @@
+#Category 1 method no. 4
+#binary converter 
+def binary(num)
+    num.to_s(2)
+end
+#octal converter
+def octal(num)
+    num.to_s(8)
+end
+#hexadecimal converter
+def hexadecimal(num)
+    num.to_s(16)
+end
+
+
+#Category 2 methods no. 4
+gi
